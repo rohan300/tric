7d4fa6b7e7b90321f59c9140f68edacacc69b55e
Bhavith Manapoty <catchbhav2002@gmail.com>
2025-04-15 16:53:44 -0400

    header done

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import React from "react";
+import Logo from "../assets/Tric Logo.svg";
 
 export const NavigationHeader: React.FC = () => {
   return (
-    <header className="flex overflow-hidden flex-col justify-center items-start px-16 py-4 max-w-full text-3xl leading-none text-teal-600 whitespace-nowrap bg-white bg-blend-normal shadow-[0px_8px_10px_rgba(0,0,0,0.03)] w-[1440px] max-md:px-5">
-      <div className="flex gap-0.5">
+    <header className="w-full flex justify-between items-center py-4 px-4 sm:px-6 lg:px-8 text-teal-600 bg-white shadow-sm">
+      <div className="flex items-center">
         <img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/d13111606df3bf924441aba99b00dcddd73c8024?placeholderIfAbsent=true&apiKey=2b75df27c5484db98535b11b9db3c47c"
-          className="object-contain shrink-0 w-10 aspect-[1.11]"
+          src={Logo}
+          className="h-8 sm:h-10 object-contain"
           alt="Tric logo"
         />
-        <h1 className="my-auto">Tric</h1>
       </div>
     </header>
   );
